0935fd0354bf58aa6fc2149431f1edc2f445229a
Junxian Xie <xie3507@126.com>
2017-08-21 22:34:44 +0800

    thank you!

---
+++ manuscript/array.js
@@ -0,0 +1,18 @@
+let items = new Array(2);
+console.log(items.length);          // 2
+console.log(items[0]);              // undefined
+console.log(items[1]);              // undefined
+
+items = new Array("2");
+console.log(items.length);          // 1
+console.log(items[0]);              // "2"
+
+items = new Array(1, 2);
+console.log(items.length);          // 2
+console.log(items[0]);              // 1
+console.log(items[1]);              // 2
+
+items = new Array(3, "2");
+console.log(items.length);          // 2
+console.log(items[0]);              // 3
+console.log(items[1]);              // "2"
